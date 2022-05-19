0d8cbd6a61296ac2ab940ff40180029cc9e26619
Joseph Hobbs <josephrosshobbs@gmail.com>
2022-05-19 12:51:36 -0400

    Implemented framework for ASCII hashing; changed name to moss-oxide

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,17 @@ fn main() {
         }
     }
 
+    // TODO: Clean up newlines (CRLF vs. LF)
     let bytes: &[u8] = input.as_bytes();
 
     println!("{}", input);
     println!("{:#?}", bytes);
+}
+
+
+/// Computes Karp-Rabin hashes on the given ASCII bytestring with substrings of length `k`.
+/// 
+/// Returns a slice of 32-bit unsigned integers representing each K-R hash.
+fn hash_ascii(bytes: &[u8]) -> &[u32] {
+    &[0u32]
 }
